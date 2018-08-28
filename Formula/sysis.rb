4371bdc429245fcfa4dc5b929d1e87b563f37bd2
Robert Peteuil <robert@peteuil.com>
2018-08-28 11:39:39 -0700

    adjust

--- Formula/sysis.rb
+++ Formula/sysis.rb
@@ -1,7 +1,7 @@
 class Sysis < Formula
   depends_on "gawk"
 
-  homepage "://github.com/robertpeteuil/sysis"
+  homepage "https://github.com/robertpeteuil/sysis"
   url "https://github.com/robertpeteuil/sysis/archive/v3.7.6.tar.gz"
   version "3.7.6"
   sha256 "fb340a136179f975eac8da095862fa2f6e09373f7ded257e4de7e6fc2e21d8dc"
@@ -9,7 +9,7 @@ class Sysis < Formula
   bottle :unneeded
 
   def install
-    bin.install "sysis"
+    bin.install "./sysis"
   end
 
   test do
