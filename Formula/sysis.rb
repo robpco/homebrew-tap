80596babf86a92a97dca3889066a541b66d3da97
Robert Peteuil <robert@peteuil.com>
2018-08-28 09:35:02 -0700

    test formula

---
+++ Formula/sysis.rb
@@ -0,0 +1,14 @@
+class Sysis < Formula
+  homepage "://github.com/robertpeteuil/sysis"
+  url "https://github.com/robertpeteuil/sysis/archive/v3.7.6.tar.gz"
+  version "3.7.6"
+  sha256 "fb340a136179f975eac8da095862fa2f6e09373f7ded257e4de7e6fc2e21d8dc"
+
+  def install
+    if File.file?("#{bin}/sysis") then
+      system "rm", "#{bin}/sysis"
+    end
+    bin.install "sysis"
+  end
+
+end
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# homebrew-tap
+# Robert Peteuil's custom Homebrew formulas
+
+## sysis - lightweight system-information utility
+
+### Installing
+
+```bash
+$ brew tap robertpeteuil/tap
+$ brew install sysis
+
+# or
+$ brew install robertpeteuil/tap/sysis
+```
+
+### SYSIS repo and usage documentation
+
+[github.com/robertpeteuil/sysis](https://github.com/robertpeteuil/sysis)
