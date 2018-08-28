eb467f583b4d92d12fddef78a598e5bdbd11179c
Robert Peteuil <robert@peteuil.com>
2018-08-28 10:57:53 -0700

    revise

--- Formula/sysis.rb
+++ Formula/sysis.rb
@@ -6,11 +6,15 @@ class Sysis < Formula
   version "3.7.6"
   sha256 "fb340a136179f975eac8da095862fa2f6e09373f7ded257e4de7e6fc2e21d8dc"
 
+  bottle :unneeded
+
   def install
-    if File.file?("#{bin}/sysis") then
-      system "rm", "#{bin}/sysis"
-    end
     bin.install "sysis"
+  end
+
+  test do
+    output = shell_output("#{bin}/sysos -h 2>&1", 1)
+    assert_match "system", output
   end
 
 end
