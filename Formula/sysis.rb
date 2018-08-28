bf7442b2775dbce1cdb77b2ac665dd869ed0052c
Robert Peteuil <robert@peteuil.com>
2018-08-28 10:52:07 -0700

    switch to bin

--- Formula/sysis.rb
+++ Formula/sysis.rb
@@ -1,14 +1,16 @@
 class Sysis < Formula
+  depends_on "gawk"
+
   homepage "://github.com/robertpeteuil/sysis"
   url "https://github.com/robertpeteuil/sysis/archive/v3.7.6.tar.gz"
   version "3.7.6"
   sha256 "fb340a136179f975eac8da095862fa2f6e09373f7ded257e4de7e6fc2e21d8dc"
 
   def install
-    if File.file?("#{etc}/sysis") then
-      system "rm", "#{etc}/sysis"
+    if File.file?("#{bin}/sysis") then
+      system "rm", "#{bin}/sysis"
     end
-    etc.install "sysis"
+    bin.install "sysis"
   end
 
 end
