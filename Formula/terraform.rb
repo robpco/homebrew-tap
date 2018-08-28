cb9eb339647aa605763f9fa7d7b54070e5d82d30
Robert Peteuil <robert@peteuil.com>
2018-08-28 11:57:57 -0700

    test tf

---
+++ Formula/terraform.rb
@@ -0,0 +1,18 @@
+class Sysis < Formula
+  homepage "https://github.com/robertpeteuil/terraform-installer"
+  url "https://github.com/robertpeteuil/terraform-installer/archive/v1.3.0.tar.gz"
+  version "1.3.0"
+  sha256 "3c16ddc5c739d60e50c9eae18ceb62bc2627f5ea6cc5c39a3422898947b27799"
+
+  bottle :unneeded
+
+  def install
+    system "./terraform-install.sh -a"
+  end
+
+  test do
+    output = shell_output("#{bin}/terraform version 2>&1", 1)
+    assert_match "Terraform", output
+  end
+
+end
