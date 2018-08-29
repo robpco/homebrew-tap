c2cb4c9e6050ec8eceeb841a1ef077d02cb21317
Robert Peteuil <robert@peteuil.com>
2018-08-29 13:44:16 -0700

    add dev, head and test

--- Formula/sysis.rb
+++ Formula/sysis.rb
@@ -3,18 +3,24 @@ class Sysis < Formula
 
   homepage "https://github.com/robertpeteuil/sysis"
   url "https://github.com/robertpeteuil/sysis/archive/v3.7.6.tar.gz"
-  version "3.7.6"
   sha256 "fb340a136179f975eac8da095862fa2f6e09373f7ded257e4de7e6fc2e21d8dc"
+  version "3.7.6"
 
+  head "https://github.com/robertpeteuil/sysis.git"
+  
   bottle :unneeded
+
+  # devel block only executed if the user passes `--devel`
+  devel do
+    url "https://github.com/robertpeteuil/sysis.git", :branch => "develop"
+  end
 
   def install
     bin.install "./sysis"
   end
 
   test do
-    output = shell_output("#{bin}/sysis -h", 1)
-    assert_match "system", output
+    system bin/"sysis", "-h"
   end
 
 end
