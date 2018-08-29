3253210c64fcc15eff46755298a65525df9005c4
Robert Peteuil <robert@peteuil.com>
2018-08-29 13:14:28 -0700

    test tf directly

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,14 +1,16 @@
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/robertpeteuil/terraform-installer/archive/v1.3.2.tar.gz"
+  url "https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_darwin_amd64.zip"
+  sha256 "98c168b06e8b4058c66e044e3744d49956ce7bc3664dc1679a33f8fffc84564d"
+  # url "https://github.com/robertpeteuil/terraform-installer/archive/v1.3.2.tar.gz"
+  # sha256 "ea374dba5eaf4c078f1f1c01fca9dacba3da7f7daf952ceb249c6b6fc50eb92a"
   version "0.11.8"
-  sha256 "ea374dba5eaf4c078f1f1c01fca9dacba3da7f7daf952ceb249c6b6fc50eb92a"
 
   bottle :unneeded
 
   def install
-    system "./terraform-install.sh", "-c", "-i", "0.11.8"
+    # system "./terraform-install.sh", "-c", "-i", "0.11.8"
     bin.install "./terraform"
   end
 
