92fbfeedc4298ca0410048fe8e1b92d158f13667
Robert Peteuil <robert@peteuil.com>
2018-12-12 11:29:50 -0700

    fix sha for 1.3.3

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -5,10 +5,10 @@ class Packer < Formula
   sha256 "5b160b82d36d8817ce57881569e3e838291595a7c4750effc5ff45cc280a32c6" if OS.linux?
   # on macOS - download 32_bit version directly
   url "https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_darwin_386.zip" if (OS.mac? && Hardware::CPU.is_32_bit?)
-  sha256 "fca84e4c1997e5563c95df4cff52d79150efc85aef755c931cf525e39c68d9a1" if (OS.mac? && Hardware::CPU.is_32_bit?)
+  sha256 "ff6eedff50bce1beec5415d482b2c610ee0eeb0d1c221e77b06e57fd1f2c2ecb" if (OS.mac? && Hardware::CPU.is_32_bit?)
   # on macOS - download 64_bit version directly
   url "https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_darwin_amd64.zip" if (OS.mac? && Hardware::CPU.is_64_bit?)
-  sha256 "78c13400b76ba0c7105ba00c99fe99dd7778100bbdf93852ae083efb99261638" if (OS.mac? && Hardware::CPU.is_64_bit?)
+  sha256 "6ffbb13bee76e02f7b065955307d454b14773feee9fcfc3b5dd96ed154b931c4" if (OS.mac? && Hardware::CPU.is_64_bit?)
   version "1.3.3"
 
   bottle :unneeded
