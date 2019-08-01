f7e0cdfb3a1b9af07bb6eef788c2660417eca41f
Robert Peteuil <robert@peteuil.com>
2019-08-01 08:20:52 +0000

    update for Terraform v0.12.6

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -4,15 +4,15 @@ class Terraform < Formula
   url "https://github.com/robertpeteuil/terraform-installer/archive/v1.5.1.tar.gz" if OS.linux?
   sha256 "a1c32fa96227499d30042a851f34e92e648b8965d600f81a87dc9a5560b01c34" if OS.linux?
   # on macOS - download directly - installer not required as amd_64 is only version available
-  url "https://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_darwin_amd64.zip" if OS.mac?
-  sha256 "e0afcf6f6401e9eaab0be588b55b5226549253854acc1d0cde331b8ca54727e0" if OS.mac?
-  version "0.12.5"
+  url "https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_darwin_amd64.zip" if OS.mac?
+  sha256 "7168dfa057d9aed7ea3f111d87294f263e341c8b848e776bc13d169ddf2926c7" if OS.mac?
+  version "0.12.6"
 
   bottle :unneeded
 
   def install
     if OS.linux?
-      system "./terraform-install.sh", "-c", "-i", "0.12.5"
+      system "./terraform-install.sh", "-c", "-i", "0.12.6"
     end
     bin.install "./terraform"
   end
