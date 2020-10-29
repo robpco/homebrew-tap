d0c35a87cff81ad3be7014885e1df87c602d1ef9
Robert Peteuil <robert@peteuil.com>
2020-10-29 07:24:30 -0700

    Update sysis.rb

--- Formula/sysis.rb
+++ Formula/sysis.rb
@@ -3,7 +3,7 @@ class Sysis < Formula
 
   homepage "https://github.com/robertpeteuil/sysis"
   url "https://github.com/robertpeteuil/sysis/archive/3.7.17.tar.gz"
-  sha256 "66312b19d4c24745caa2ff2830d2cecb82d5b834fa4eb3e0b0d7f6bc3f68942a"
+  sha256 "5d1bcf7a85f36940d2ad598920bf63ce6a36cdf24898b925b6e4b810444fc9b2"
   version "3.7.17"
 
   head "https://github.com/robertpeteuil/sysis.git"
